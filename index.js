0d8f1b3a15723eeb79ffd33e2be463f3bc7047a0
Sean Darcy <darcys22@gmail.com>
2015-08-31 23:47:09 +1000

    stuff isn't working inside the find prob

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ var db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function (callback) {
+  console.log("writing stuff");
 
   console.log(calculator(mongoose));
   process.exit();
