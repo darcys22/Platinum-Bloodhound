745dec500b5fc75007e9777fac917e1f4068748e
Sean Darcy <darcys22@gmail.com>
2015-09-01 19:42:31 +1000

    returns an array of objects with votes and rank

--- index.js
+++ index.js
@@ -1,22 +1,39 @@
 // modules =================================================
 var mongoose       = require('mongoose');
 var calculator     = require('./lib/');
+var mongo          = require('mongodb').MongoClient;
+var assert         = require('assert');
+var _              = require('underscore');
 // configuration ===========================================
 	
 // config files
 var dbconfig = require('./config/db');
 
+mongoose.connect(dbconfig.url);
 
-var db = mongoose.connection;
+var Schema = mongoose.Schema;
 
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function (callback) {
-  console.log("writing stuff");
-
-  console.log(calculator(mongoose));
-  process.exit();
-
+var UserSchema = new Schema({
+  id : {type : String, required: true},
+  email : {type : String},
+  books : [{type: Schema.Types.ObjectId}]
 });
 
-mongoose.connect(dbconfig.url); // connect to our mongoDB database
 
+var User = mongoose.model('User', UserSchema);
+
+User.find({}, 'books', function (err, users) {
+  assert.equal(null, err);
+
+  var top100 = _.chain(users)
+                .reduce( function (memo, objek) { return memo.concat(objek.books); }, [])
+                .countBy(_.identity)
+                .map( function(value, key) { return { _id : key, votes : value } })
+                .sortBy(function(item) { return item.votes * -1; } )
+                .first(100)
+                .forEach(function(item, i) { item.rank = (i + 1); } )
+                .value();
+
+  console.log(top100);
+  process.exit();
+});
