051dd6599d6df648c6f3de8f82c4cc464b811852
Sean Darcy <darcys22@gmail.com>
2015-09-01 19:50:32 +1000

    moved the calculator to its own file

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // modules =================================================
 var mongoose       = require('mongoose');
 var calculator     = require('./lib/');
-var mongo          = require('mongodb').MongoClient;
+//var mongo          = require('mongodb').MongoClient;
 var assert         = require('assert');
-var _              = require('underscore');
+//var _              = require('underscore');
 // configuration ===========================================
 	
 // config files
@@ -25,14 +25,7 @@ var User = mongoose.model('User', UserSchema);
 User.find({}, 'books', function (err, users) {
   assert.equal(null, err);
 
-  var top100 = _.chain(users)
-                .reduce( function (memo, objek) { return memo.concat(objek.books); }, [])
-                .countBy(_.identity)
-                .map( function(value, key) { return { _id : key, votes : value } })
-                .sortBy(function(item) { return item.votes * -1; } )
-                .first(100)
-                .forEach(function(item, i) { item.rank = (i + 1); } )
-                .value();
+  var top100 = calculator(users);
 
   console.log(top100);
   process.exit();
