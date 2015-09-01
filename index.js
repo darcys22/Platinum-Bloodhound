92c2e5f9388adb959f3a80fc8580843a2e4e3772
Sean Darcy <darcys22@gmail.com>
2015-09-01 23:07:59 +1000

    works but needs to be scheduled and backgrounded

--- index.js
+++ index.js
@@ -10,10 +10,21 @@ var User = library.User;
 var Book = library.Book;
 
 User.find({}, 'books', function (err, users) {
+
   assert.equal(null, err);
 
   var top100 = library.calculator(users);
 
-  console.log(top100);
-  process.exit();
+  Book.update({}, {rank: 0, votes: 0}, {multi: true}, function(err) { 
+    assert.equal(null, err) 
+
+    library.save(top100, Book).then(function(bulkRes){
+      console.log('Bulk complete.');
+      process.exit();
+    }, function(err){
+        console.log('Bulk Error:', err);
+        process.exit();
+    });
+  });
+
 });
